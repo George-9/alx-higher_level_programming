1d0128f4c847644b57a65336cd851565bc14cb96
George-9 <georgemuigainjau@gmail.com>
2022-06-01 19:27:55 +0300

    formating in print statement - formatting output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ l_p = "and is less than 6 and not 0"
 if l_digit == 0:
     print("Last digit of {:d} is {l_digit:d} and is 0".format(number))
 elif number < 0:
-    print(f"{p} {:d} is {l_digit * -1:d} {l_p}".format(number))
+    print("{p} {:d} is {l_digit * -1:d} {l_p}".format(number))
 elif number > 0 and l_digit > 5:
-    print(f"{p} {:d} is {l_digit:d} and is greater than 5".format(number))
+    print("{p} {:d} is {l_digit:d} and is greater than 5".format(number))
 else:
-    print(f"{p} {:d} is {l_digit:d} {l_pp}".format(number))
+    print("{p} {:d} is {l_digit:d} {l_pp}".format(number))
