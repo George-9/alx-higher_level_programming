bfe18178212bb31d223cf131f9dc0e6d58fa310c
George-9 <georgemuigainjau@gmail.com>
2022-06-01 10:10:54 +0300

    loops and item separators

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02d}".format(i), end=", ")
+#for i in range(100):
+#    print("{:02d}".format(*range(100)), sep=", ")
+print(*range(100), sep=", ")
