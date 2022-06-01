e7987a05d74911ca3db85587ff7e9b125214d699
George-9 <georgemuigainjau@gmail.com>
2022-06-01 13:39:14 +0300

    continue check satetment

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(1, 89):
     if i >= 10:
         if i > 10 and ((str(i)[0] == str(i)[1])):
             continue
-        if int((str(i)[1] + str(i)[0])) > (int((str(i)[0] + str(i)[1]))):
+        if int((str(i)[1] + str(i)[0])) < i:
             continue
     print("{:02d}".format(i), end=", ")
 print("{:02d}".format(i + 1))
