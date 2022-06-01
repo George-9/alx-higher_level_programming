0d1b2c20cb57d1caaa05bb553f561da1c81a7343
George-9 <georgemuigainjau@gmail.com>
2022-06-01 19:39:04 +0300

    formating in print statement - formatting output {} and format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l_digit = int(str(number)[(len(str(number))) - 1])
+l_digit = int(str(number)[-1])
 l_pp = "and is less than 6 and is not 0"
 p = "Last digit of"
 l_p = "and is less than 6 and not 0"
@@ -12,4 +12,4 @@ elif number < 0:
 elif number > 0 and l_digit > 5:
     print("{} {:d} is {:d} and is greater than 5".format(p, number, l_digit))
 else:
-    print("{p} {:d} is {:d} {}".format(p, number, l_digit, l_pp))
+    print("{} {:d} is {:d} {}".format(p, number, l_digit, l_pp))
