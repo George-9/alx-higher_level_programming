64fac3ef0c27c2ed20e51034a8ccc17f9cf72d53
George-9 <georgemuigainjau@gmail.com>
2022-06-01 08:45:36 +0300

    a program that check and prints the last digit of a randomly generated number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int((str(number))[(len(str(number))) - 1])
-print(number)
-
-if last_digit == 0:
-	print(f"Last digit of {number:d} is {last_digit:d} and is 0")
-
+l_digit = int((str(number))[(len(str(number))) - 1])
+p = "Last digit of"
+if l_digit == 0:
+    print(f"Last digit of {number:d} is {l_digit:d} and is 0")
 elif number < 0:
-	print(f"Last digit of {number:d} is {last_digit * -1:d} and is less than 6 and is not 0")
-
-elif number > 0 and last_digit > 5:
-	print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
+    print(f"{p} {number:d} is {l_digit * -1:d} and is less than 6 and is not 0")
+elif number > 0 and l_digit > 5:
+    print(f"{p} {number:d} is {l_digit:d} and is greater than 5")
 else:
-        print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and is not 0")
-
+    print(f"{p} {number:d} is {l_digit:d} and is less than 6 and is not 0")
