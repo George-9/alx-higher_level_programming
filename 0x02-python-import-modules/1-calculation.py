ef288b800211b4b0af79502e1c1cbf4f6a650804
George-9 <georgemuigainjau@gmail.com>
2022-06-02 13:17:43 +0300

    multiple imports

--- 0x02-python-import-modules/1-calculation.py
+++ 0x02-python-import-modules/1-calculation.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-if __name__ == "__main__":
+if __name__ == '__main__':
     print("{:d}".format(add(a, b)))
     print("{:d}".format(sub(a, b)))
     print("{:d}".format(mul(a, b)))
