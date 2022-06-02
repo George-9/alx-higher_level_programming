a9b14e35bf7fca4d8ec18f9323b142f80ad3bbcb
George-9 <georgemuigainjau@gmail.com>
2022-06-02 22:14:23 +0300

    a program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == '__main__':
-    n = 1
+    n = 0
     if len(argv) == 1:
         print('0 arguments.')
     if len(argv) == 2:
         print('1 argument:')
-        print('{:d}: {}'.format(1, argv[1]))
+        print('{:d}: {:d}'.format(1, argv[1]))
     else:
+        print('{:d} arguments:'.format(len(argv) - 1))
         for arg in argv:
-            if n > 1:
-                print('{:d}: {}'.format(n, arg))
-                n += 1
+            if n >= 1:
+                print('{:d}: {:d}'.format(n, int(arg)))
+            n += 1
