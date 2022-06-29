bd626aa7a51d1c4363f4ce6c97d7ea5331a74d9e
George-9 <georgemuigainjau@gmail.com>
2022-06-29 18:16:44 +0300

    a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+   a simple Square module
+"""
+
+
+class Square:
+    """ This is a Square class that has private attributes or properties
+        Attributes:
+            __size (str): private attribute
+    """
+    def __init__(self, _Square__size=0):
+        self.size = _Square__size
+
+    def area(self):
+        if ((type(self.size)) is not int):
+            raise TypeError("size must be an integer")
+
+        if (self.size < 0):
+            raise ValueError("size must be >= 0")
+
+        return self.size * self.size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        self.__size = value
+
+    def my_print(self):
+        if (self.size == 0):
+            print()
+        for(i in range(self.size)):
+            print("#" * self.size)
