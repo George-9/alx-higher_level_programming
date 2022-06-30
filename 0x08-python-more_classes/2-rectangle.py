7e47ae3a00236b5d09356968fdbd92ecaf775655
George-9 <georgemuigainjau@gmail.com>
2022-06-30 06:25:39 +0300

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py). Defines area and perimeter methods for the rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+    a simple python module about a rectangle class
+"""
+
+
+class Rectangle:
+    """
+        Attributes:
+    """
+    def __init__(self, width=0, height=0):
+        self._Rectangle__height = height
+        self._Rectangle__width = width
+
+        if (width < 0):
+            ValueError("raise width must be >= 0")
+        if type(self._Rectangle__width) is not int:
+            raise TypeError("height must be an integer")
+        if (self._Rectangle__height < 0):
+            raise ValueError("height must be >= 0")
+        if type(self._Rectangle__height) is not int:
+            raise TypeError("height must be an integer")
+        if (self._Rectangle__height < 0):
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self._Rectangle__width
+
+    @width.setter
+    def width(self, value):
+        if type(self._Rectangle__width) is not int:
+            raise TypeError("width must be an integer")
+        if (self._Rectangle__width < 0):
+            raise ValueError("width must be >= 0")
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        return self._Rectangle__height
+
+    @width.setter
+    def height(self, value):
+        if type(self._Rectangle__height) is not int:
+            raise TypeError("height must be an integer")
+        if (self._Rectangle__height < 0):
+            raise ValueError("height must be >= 0")
+        self._Rectangle__height = value
+
+    def area(self):
+        return self._Rectangle__width * self._Rectangle__height
+
+    def perimeter(self):
+        if self._Rectangle__width == 0 or self._Rectangle__height == 0:
+            return 0
+        return (self._Rectangle__width * 2) + (self._Rectangle__height * 2)
