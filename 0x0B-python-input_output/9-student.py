3e7ccf7d6100f84bb1c570467ddc5e5872abf10b
George-9 <georgemuigainjau@gmail.com>
2022-07-16 11:58:49 +0300

    a class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    a module for a Student class 
+"""
+class Student(dict):
+    """
+        Attributes:
+            first_name - student's first name
+            last_name - student's last name
+            age - students age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            initialize object and set it's properties
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """
+            a method that returns a json representation of
+            the student object
+        """
+        return json.loads(self)
