dfd9e1a0da8bccded71fab7b16dad8860b9d7e01
George-9 <georgemuigainjau@gmail.com>
2022-07-30 09:10:52 +0300

    a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,72 +1,85 @@
 #!/usr/bin/python3
 """
-    This module defines a rectanlge class
-
-    It also includes methods for the rectangle
+    a simple python module about a rectangle class
 """
 
 
 class Rectangle:
-    number_of_instances = 0
     """
         Attributes:
             number_of_instances (int): count number of class instances
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
-
         self._Rectangle__height = height
         self._Rectangle__width = width
-
         self.symbol = "#"
 
     @property
     def width(self):
+        """
+           returns the width of the rectangle
+        """
         return self._Rectangle__width
 
     @width.setter
     def width(self, value):
+        """
+           sets the width of the rectangle
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-
-        if (value < 0):
+        if (self._Rectangle__width < 0):
             raise ValueError("width must be >= 0")
-
         self._Rectangle__width = value
 
     @property
     def height(self):
+        """
+           returns the height of the rectangle
+        """
         return self._Rectangle__height
 
     @height.setter
     def height(self, value):
-        if type(val) is not int:
+        """
+           sets the height of the Rcanlge object
+        """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-
-        if (value < 0):
+        if (self._Rectangle__height < 0):
             raise ValueError("height must be >= 0")
-
         self._Rectangle__height = value
 
     def area(self):
+        """
+           returns the area of the Rectangle
+        """
         return self._Rectangle__width * self._Rectangle__height
 
     def perimeter(self):
+        """
+           returns the perimeter of the Rectangle object
+        """
         if self._Rectangle__width == 0 or self._Rectangle__height == 0:
             return 0
         return (self._Rectangle__width * 2) + (self._Rectangle__height * 2)
 
     @property
     def print_symbol(self):
+        """
+            returns the currently set symbol for the object
+        """
         return self.symbol
 
     @print_symbol.setter
     def print_symbol(self, val):
+        """
+            sets the symbol for the object
+        """
         self.symbol = val
-
-    def __repr__(self):
-        w, h = self._Rectangle__width, self._Rectangle__height
-        return f'Rectangle({w}, {h})'
 
     def __str__(self):
         if self._Rectangle__width == 0 or self._Rectangle__height == 0:
@@ -74,11 +87,15 @@ class Rectangle:
         rect = ''
         cont = ''
         for i in range(self._Rectangle__height):
-            rect += (str(self.print_symbol) * self._Rectangle__width)
+            rect += ((str(self.print_symbol)) * self._Rectangle__width)
             rect += '\n'
         for a in range(len(rect) - 1):
             cont += rect[a]
         return cont
+
+    def __repr__(self):
+        w, h = self._Rectangle__width, self._Rectangle__height
+        return f'Rectangle({w}, {h})'
 
     def __del__(self):
         print("Bye rectangle...")
